11b97afe0747b989696666b0fb016de4d262fd7e
nvithala <vithala.niharika@gmail.com>
2017-03-20 10:38:09 -0400

    Initial Commit

---
+++ GreenRoute/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GreenRoute
+//
+//  Created by Vithala,Niharika on 3/20/17.
+//  Copyright © 2017 Vithala,Niharika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
